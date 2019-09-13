d81727038ca62a5659d4852e94d2d7e22d63c38e
Li Arolf Rey <liarolfrrey@gmail.com>
2019-09-13 15:29:11 +0800

    feat: Add useMedia & debounce utils

---
+++ client/src/utils/debounce.js
@@ -0,0 +1,18 @@
+function debounce(func, wait, immediate) {
+  let timeout = null;
+  return function run() {
+    const context = this;
+    const args = arguments;
+    const executable = immediate && !timeout;
+    clearInterval(timeout);
+    timeout = setTimeout(() => {
+      timeout = null;
+      if (!executable) {
+        func.apply(context, args);
+      }
+    }, wait);
+    if (executable) func.apply(context, args);
+  };
+}
+
+export default debounce;
