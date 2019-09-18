c8bd6b7f02eedb9cc69e6146dade4b7664d2956c
Li Arolf Rey <liarolfrrey@gmail.com>
2019-09-18 15:07:26 +0800

    feat: Refractor useMedia to use context to pass media

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,33 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { StyledNav, NavLinks, PostGigButton } from './style';
+import { MediaConsumer } from '../MediaProvider';
 import NavLink from '../NavLink';
 
-const Nav = ({ size }) => {
+const Nav = () => {
   return (
-    <StyledNav>
-      <NavLink href="/">The Gig Atlas</NavLink>
-      {size === 'desktop' || size === 'giant' || !size ? (
-        <NavLinks>
-          <NavLink href="/freelancers">Freelancers</NavLink>
-          <NavLink href="/gigs">Find Gigs</NavLink>
-          <NavLink href="/gig/software/create">
-            <PostGigButton>Post A Gig</PostGigButton>
-          </NavLink>
-        </NavLinks>
-      ) : (
-        <div>Menu</div>
+    <MediaConsumer>
+      {({ size }) => (
+        <StyledNav>
+          <NavLink href="/">The Gig Atlas</NavLink>
+          {size === 'desktop' || size === 'giant' || !size ? (
+            <NavLinks>
+              <NavLink href="/freelancers">Freelancers</NavLink>
+              <NavLink href="/gigs">Find Gigs</NavLink>
+              <NavLink href="/gig/software/create">
+                <PostGigButton>Post A Gig</PostGigButton>
+              </NavLink>
+            </NavLinks>
+          ) : (
+            <div>Menu</div>
+          )}
+        </StyledNav>
       )}
-    </StyledNav>
+    </MediaConsumer>
   );
-};
-
-Nav.propTypes = {
-  size: PropTypes.string,
-};
-
-Nav.defaultProps = {
-  size: 'giant',
 };
 
 export default Nav;
