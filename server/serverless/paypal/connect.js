0fab2d8847ceae21d641e47127b02cd0cbe59e23
jansel369 <dianzel555@gmail.com>
2019-10-17 19:03:03 +0800

    chore(server checkout): cleanup debug tools and comments

--- server/serverless/paypal/connect.js
+++ server/serverless/paypal/connect.js
@@ -15,24 +15,13 @@ if (!clientId || !clientSecret || !uri) {
   process.exit(1);
 }
 
-export const request = axios.create({
+const request = axios.create({
   baseURL: uri,
   headers: {
-    // Authorization: `Basic ${clientId}:${clientSecret}`,
-    // Authorization:
-    // 'Bearer A21AAG5OJxFnteBHPvXD2YsLqxeHQWPFEGTQoxXO49ObCa6Y1CVjA6TlhVixdVRbTTrQcTpWdi9bEuaLuMoZLFiryyEQlK7gQ',
     'Content-Type': 'application/json',
-    // 'Content-type': 'application/x-www-form-urlencoded',
     'Accept-Language': 'en_US',
   },
   auth: { username: clientId, password: clientSecret },
 });
 
-/**
- * @todo: to be set staticly on client
- */
-export function getCDN() {
-  return `https://www.paypal.com/sdk/js?currency=USD&client-id=${
-    isDev ? 'sb' : clientId // => set sandbox (sb) on dev
-  }`;
-}
+export default request;
--- server/serverless/paypal/index.js
+++ server/serverless/paypal/index.js
@@ -1,50 +1,7 @@
-/* eslint-disable no-console */
-/* eslint-disable import/named */
-// import util from './util';
-import { getCDN } from './connect';
-
-// let auth;
-
-// /**
-//  * Handle on payment completion
-//  * @param {String} id order id
-//  */
-// export async function completePayment(id) {
-//   const
-// }
-
-/**
- * Query oauth2 token from paypal
- * @returns Object { access_token, token_type, app_id, expires_in, nonce, scope(omitted) }
- */
-// export async function getToken() {
-//   // => paypal only supports v1 oauth2
-//   const url = `${uri}/v1/oauth2/token?grant_type=client_credentials`;
-//   const config = {
-//     headers: {
-//       Accept: 'application/json',
-//       'Content-type': 'application/x-www-form-urlencoded',
-//       'Accept-Language': 'en_US',
-//     },
-//     auth: { username: clientId, password: clientSecret },
-//   };
-//   try {
-//     const { data } = await axios.post(url, {}, config);
-//     // eslint-disable-next-line no-unused-vars
-//     const { scope, ...rest } = data;
-//     console.log(rest);
-//     auth = rest;
-
-//     return auth;
-//   } catch (e) {
-//     console.log('ERROR on paypal authentication');
-//     console.log(e.response.data);
-
-//     throw e;
-//   }
-// }
+import request from './connect';
+import order from './order';
 
 export default {
-  getCDN,
-  // getToken,
+  request,
+  order,
 };
