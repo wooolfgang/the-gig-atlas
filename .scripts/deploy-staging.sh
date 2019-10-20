91338da8e9dac484a35727df72e5d0f083b79fe9
Li Arolf Rey <liarolfrrey@gmail.com>
2019-10-20 12:04:30 +0800

    Update deployment script to exit on error

--- .scripts/deploy-staging.sh
+++ .scripts/deploy-staging.sh
@@ -1,11 +1,44 @@
--#!/bin/sh     
-sshpass -p $SSH_PASS ssh -o StrictHostKeyChecking=no dev@167.179.102.140 <<EOF       
-  cd the-gig-atlas && git pull origin develop
-  npm run bootstrap && npm run common:build
+-#!/bin/sh   
+
+# Exit script on error
+set -e
+printenv
+
+#GITHUB_HEAD_REF -> The current branch that is being merged
+#GITHUB_BASE_REF -> The base branch when you PR, usually it's the develop branch
+
+sshpass -p $SSH_PASS ssh -o StrictHostKeyChecking=no dev@167.179.102.140 <<EOF
+  cd the-gig-atlas && git checkout ${GITHUB_BASE_REF}
+  
+  # Make sure code is up-to-date with the latest of the base origin
+  # Remove untracked files
+  git fetch origin
+  git add --all
+  git reset --hard origin/${GITHUB_BASE_REF}
+  git pull origin ${GITHUB_HEAD_REF}
+
+  # Install project dependencies
+  npm run bootstrap
+
+  # Copy all the environment variables for staging
   cp server/.env.staging server/.env
-  npm run server:prisma
   cp client/.env.staging client/.env
+
+  # Update prisma db to latest datamodel, generate prisma schema
+  npm run server:prisma
+  npm run server:prisma-generate
+
+  # Building the common package
+  npm run common:build
+
+  # Building the server package
+
+  # Building the client package
   npm run client:build
-  pm2 restart client && pm2 restart server
-  exit  
+
+  # Restarting pm2 instances
+  pm2 restart client:staging && pm2 restart server:staging
+
+  echo Build process executed successfully!
+  exit
 EOF
