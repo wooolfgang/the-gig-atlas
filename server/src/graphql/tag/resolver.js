316aec701f3103d2482b6f45c41fad14b8c80b09
Li Arolf Rey <liarolfrrey@gmail.com>
2019-11-22 06:51:20 +0800

    feat: tags and tag resolvers

--- server/src/graphql/tag/resolver.js
+++ server/src/graphql/tag/resolver.js
@@ -1,4 +1,18 @@
+import prisma from '../../prisma';
+
+const tags = (_, args, _ctx, info) => prisma.tags(args, info);
+const tag = (_, { id }, _ctx, info) => prisma.tag({ id }, info);
+
 export default {
-  Query: {},
+  Query: {
+    tag,
+    tags,
+  },
   Mutation: {},
+  Tag: {
+    category: ({ id }) => prisma.tag({ id }).category(),
+  },
+  TagCategory: {
+    tags: ({ id }) => prisma.tagCategory({ id }).tags(),
+  },
 };
