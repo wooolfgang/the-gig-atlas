efc192ec16300e44dc5d261523c9daa0fae0b67f
Li Arolf Rey <liarolfrrey@gmail.com>
2019-11-22 16:04:09 +0800

    refactor: Move resolvers to separate functions

---
+++ server/src/graphql/thread/resolvers/createThread.js
@@ -0,0 +1,17 @@
+import prisma from '../../../prisma';
+import { sortExistTags } from '../../tag/util';
+
+export default async function createThread(_, { input }, { user }, info) {
+  const { title, body, tags } = input;
+  const createOrConnect = await sortExistTags(tags);
+
+  return prisma.createThread(
+    {
+      title,
+      body,
+      tags: createOrConnect,
+      postedBy: { connect: { id: user.id } },
+    },
+    info,
+  );
+}
---
+++ server/src/graphql/thread/resolvers/upvoteComment.js
@@ -0,0 +1,48 @@
+import prisma from '../../../prisma';
+import { DOWNVOTE_VALUE, UPVOTE_VALUE } from '../constants';
+
+export default async function upvoteComment(_, { commentId }, { user }, info) {
+  const comment = await prisma.comment({ id: commentId });
+  const { upvoteCount, downvoteCount } = comment;
+  const [downvote] = await prisma.commentVotes({
+    where: {
+      user: {
+        id: user.id,
+      },
+      value: DOWNVOTE_VALUE,
+    },
+  });
+
+  const upsert = {
+    where: {
+      id: downvote ? downvote.id : '',
+    },
+    update: {
+      value: UPVOTE_VALUE,
+    },
+    create: {
+      value: UPVOTE_VALUE,
+      user: {
+        connect: {
+          id: user.id,
+        },
+      },
+    },
+  };
+
+  return prisma.updateComment(
+    {
+      where: {
+        id: commentId,
+      },
+      data: {
+        votes: {
+          upsert,
+        },
+        upvoteCount: upvoteCount + 1,
+        downvoteCount: downvote ? downvoteCount - 1 : downvoteCount,
+      },
+    },
+    info,
+  );
+}
