d20e47b09f9d0fc942774083072623b2dc661cbf
jansel369 <dianzel555@gmail.com>
2019-11-26 11:06:11 +0800

    feat(server: file permission): add validation for image updload

--- server/src/graphql/file/permission.js
+++ server/src/graphql/file/permission.js
@@ -1,9 +1,28 @@
-import { allow } from 'graphql-shield';
+import { allow, rule } from 'graphql-shield';
+import { validation } from '@shared/common';
 import { isAuthenticated } from '../utils/rules';
+
+const isValidImage = rule()(async (_, args) => {
+  let file;
+  try {
+    file = await args.file;
+  } catch (e) {
+    return 'Image upload error';
+  }
+  try {
+    await validation.imageName.validate(file.filename);
+    // eslint-disable-next-line no-param-reassign
+    args.file = file;
+
+    return true;
+  } catch (e) {
+    return 'Invalid image type';
+  }
+});
 
 export default {
   Mutation: {
-    uploadImage: allow,
+    uploadImage: isValidImage,
     createFile: isAuthenticated,
   },
   Query: {
