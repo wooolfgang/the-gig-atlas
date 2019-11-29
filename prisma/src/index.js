4bc945f79645693314f5292f57f3d77504386d19
Li Arolf Rey <liarolfrrey@gmail.com>
2019-11-29 15:42:43 +0800

    feat: Prisma client to use .env

--- prisma/src/index.js
+++ prisma/src/index.js
@@ -1,3 +1,10 @@
+/**
+ * Injects the .env variables from where the prisma-client is run,
+ * NOT the env variables in this repository
+ * Ex: Run this on server, it will use the .env variables on server package
+ */
+require('dotenv').config();
+
 const prismaClient = require('./prisma-client/index.js');
 
-export default prismaClient.prisma;
+module.exports = prismaClient.prisma;
