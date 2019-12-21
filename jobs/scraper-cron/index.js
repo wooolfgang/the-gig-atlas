cd910e3fd66e6bfad4590e231d1fb49852a81706
Li Arolf Rey <liarolfrrey@gmail.com>
2019-12-21 13:14:22 +0800

    feat: Add slack logging for cron-jobs

--- jobs/scraper-cron/index.js
+++ jobs/scraper-cron/index.js
@@ -2,13 +2,31 @@ const express = require('express');
 const cron = require('node-cron');
 
 const app = express();
+require('dotenv').config();
+
 const seedGigs = require('./seedGigs');
+const sendSlackMessage = require('./utils/sendSlackMessage');
 
 /* Run cron job every 2 hours */
 cron.schedule('0 0 */2 * * *', async () => {
-  console.log('Running seed-gig cron task... ', new Date());
-  await seedGigs();
-  console.log('Ended seed-gig cron task...', new Date());
+  const startMessage = `Running seed-gig cron task... ${new Date()}`;
+  console.log(startMessage);
+
+  let threadTs;
+
+  try {
+    const res = await sendSlackMessage(startMessage);
+    const resJSON = await res.json();
+    threadTs = resJSON.message.ts;
+  } catch (e) {
+    console.log('Failed to send message on slack');
+  }
+
+  await seedGigs(threadTs);
+
+  const endMessage = `Ended seed-gig cron task... ${new Date()}`;
+  console.log(endMessage);
+  await sendSlackMessage(endMessage);
 });
 
 const port = 5000;
