a9bd33cdbcfbb99785e6f01cd2a1b84dff896a4a
jansel369 <dianzel555@gmail.com>
2020-01-19 16:05:50 +0800

    chore(prisma migration): update working migration

--- prisma/knexfile.js
+++ prisma/knexfile.js
@@ -53,3 +53,48 @@ module.exports = {
     // },
   },
 };
+
+/**
+ * Migrations => http://knexjs.org/#Migrations
+
+Once you have finished writing the migrations, you can update
+  the database matching your NODE_ENV by running:
+
+$ knex migrate:latest
+
+You can also pass the --env flag or set NODE_ENV to select an alternative environment:
+
+$ knex migrate:latest --env production
+
+# or
+
+$ NODE_ENV=production knex migrate:latest
+
+To rollback the last batch of migrations:
+
+$ knex migrate:rollback
+
+To rollback all the completed migrations:
+
+$ knex migrate:rollback --all
+
+To run the next migration that has not yet been run
+
+$ knex migrate:up
+
+To run the specified migration that has not yet been run
+
+$ knex migrate:up 001_migration_name.js
+
+To undo the last migration that was run
+
+$ knex migrate:down
+
+To undo the specified migration that was run
+
+$ knex migrate:down 001_migration_name.js
+
+To list both completed and pending migrations:
+
+$ knex migrate:list
+ */
